1c32e560e9e3ec417bd0c81e0fef3a5cd861f407
Ahsan-Sarbaz <geeksdotpk@gmail.com>
2019-11-03 20:34:42 +0500

    Worked on Shaders for a bit can't find motivation to do something :(

--- Jin/Shader.cpp
+++ Jin/Shader.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <iostream>
 
 Shader::Shader(const char* vspath, const char* fspath)
 {
@@ -17,8 +18,20 @@ Shader::Shader(const char* vspath, const char* fspath)
 	m_ID = glCreateProgram();
 	glAttachShader(m_ID, vsId);
 	glAttachShader(m_ID, fsId);
-
+	
 	glLinkProgram(m_ID);
+
+	int success;
+	char* infoLog;
+	glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(m_ID, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vsId);
+	glDeleteShader(fsId);
 }
 
 std::string Shader::ReadShaderFile(const char* path)
@@ -38,6 +51,50 @@ int Shader::CompileShader(const char* source, ShaderType type)
 	
 	glCompileShader(shader);
 
+	int success;
+	char* infoLog;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION\n" << infoLog << std::endl;
+	}
+
 	return shader;
+}
+
+int Shader::GetUniformLocation(const char* name)
+{
+	if (m_uniformCache.find(name) != m_uniformCache.end())
+	{
+		m_uniformCache[name] = glGetUniformLocation(m_ID, name);
+	}
+
+	return m_uniformCache[name];
+}
+
+void Shader::SetUniformInt(const char* name, int val)
+{
+	glUniform1i(GetUniformLocation(name), val);
+}
+
+void Shader::SetUniformFloat(const char* name, float x)
+{
+	glUniform1f(GetUniformLocation(name), x);
+}
+
+void Shader::SetUniformFloat(const char* name, float x, float y)
+{
+	glUniform2f(GetUniformLocation(name), x, y);
+}
+
+void Shader::SetUniformFloat(const char* name, float x, float y, float z)
+{
+	glUniform3f(GetUniformLocation(name), x, y, z);
+}
+
+void Shader::SetUniformFloat(const char* name, float x, float y, float z, float w)
+{
+	glUniform4f(GetUniformLocation(name), x, y, z, w);
 }
 
--- Jin/Shader.h
+++ Jin/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <GL/glew.h>
 #include <string>
+#include <unordered_map>
 
 enum ShaderType
 {
@@ -13,10 +14,19 @@ class Shader
 {
 private:
 	int m_ID;
+	std::unordered_map<const char*, int> m_uniformCache;
 public:
 	Shader(const char* vspath, const char* fspath);
 
 private:
 	std::string ReadShaderFile(const char* path);
 	int CompileShader(const char* source, ShaderType Type);
+
+	int GetUniformLocation(const char* name);
+
+	void SetUniformInt(const char* name, int val);
+	void SetUniformFloat(const char* name, float x);
+	void SetUniformFloat(const char* name, float x, float y);
+	void SetUniformFloat(const char* name, float x, float y, float z);
+	void SetUniformFloat(const char* name, float x, float y, float z, float w);
 };
